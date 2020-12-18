942773ac9012c1ff6ce902765cb76dce2cb6ee9b
poarfrippe <philipp.gruber02@gmail.com>
2020-12-18 01:37:43 +0100

    MitSerielScan

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # SNMPprojPy
 
 SNMP Tool in Python <br>
-SN Projekt
-<br><br>
-Bis jetzt funktioniert nur das SNMP GET von einigen vorgegebenen OIDs. Die IP-Adresse kann man in der console eingeben.
+SN Projekt<br><br>
+Es Funktioniert das Get und das Scan, wobei das scanen nicht Parallel ablaeuft, das verursachte probleme und funktionierte nicht.<br>
+Um das Programm auszufuehren bracht man python und zusaetzlich dazu die pysnmp library. Dann kann man die File snmpTest.py ausführen. <br><br>
+Als Nächstes werde ich versuchen das Scan parallel zu verarbeiten. Dann werde ich noch ein commando zum erhalten von SNMP Traps implementieren und dann mache ich noch villeicht eine GUI
--- snmpTest.py
+++ snmpTest.py
@@ -1,5 +1,35 @@
 import SnmpOperations
 import ipaddress
+import threading
+import time
+
+#ich weiß, bloßes exept nicht so gut weil es alles abfängt und man somit nicht weiß wo genau man einen fehler gemacht hat, aber fuer jetzt hab ich es mal so gemacht
+
+def getThread(iptoscan):
+    print("Thread fuer ip " + str(iptoscan)+ " gestartet")
+    try:
+        #time.sleep(1)
+        resultarray = SnmpOperations.get(iptoscan, oidarray, "public")
+        print("Informationen zum Geraet mit der IP: " + str(iptoscan))
+        j = 0;
+        print("")
+        print("")
+        for i in oidarray:
+            print(oidsname[j] + ": " + str(resultarray[i]))
+            j = j+1
+        print("")
+        print("")
+    except:
+        #print("Kann die Informationen dieser Adresse nicht abrufen.")
+        pass
+
+def prbierthread(scannip, oidarrayinfunc):
+    print("Probieren mit der IP: " + str(scannip))
+
+    gegettet = SnmpOperations.get(scannip, oidarray, "public")
+    for i in oidarrayinfunc:
+        print(str(gegettet[i]))
+    
 
 if __name__ == "__main__":
 
@@ -29,11 +59,15 @@ if __name__ == "__main__":
     oidarray.append(processnumber)
     oidarray.append(ramsize)
 
-
     while True:
         command = input("was willst du machen? Infos von bestimmter IP bekommen (get) oder ein gesamtes Netzwerk scannen (scan)")
         if command == "get" :
             ip = input("Gib IP-Adresse ein!!")
+
+            #testthread = threading.Thread(target=prbierthread, args=(ip, oidarray))
+            #testthread.start()
+            #time.sleep(2)
+
             try:
                 resultarray = SnmpOperations.get(ip, oidarray, "public")
                 j = 0;
@@ -47,9 +81,20 @@ if __name__ == "__main__":
             try:
                 if '/' not in network:
                     raise Exception
+
+                threads = []
+                i = 0
                 for ip in ipaddress.IPv4Network(network):
-                    print(ip)
+                    threads.append(threading.Thread(target=getThread, args=(str(ip),)))
+                    threads[i].start()
+                    threads[i].join()   #dauert zwar sehr sehr lange und der Thread hat keinen nutzen, so funktioniert es aber. sonst aus irgend einenm Grund nicht
+                    i = i+1
+
+                #print("now joining threads")
+                #for k in range(0, 254):
+                    #threads[k].join()
+
             except:
                 print("Etwas beim scannen ist Falsch gelaufen")
         else:
-            print("falscher Befehl!!");
+            print("falscher Befehl!!")
